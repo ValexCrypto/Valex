e88973bd5e2975cb701c5fff81c5af4d1552c0b5
khelmy <khelmy@andrew.cmu.edu>
2017-11-30 18:47:40 -0500

    Removed any mention of valex-api-demos

--- docs/valex.cc/scripts/update-demo-app-download-link.js
+++ docs/valex.cc/scripts/update-demo-app-download-link.js
@@ -6,12 +6,12 @@ module.exports = function updateDemoAppDownloadLink () {
 
   if (!platform) return
 
-  const releaseServer = 'https://valex-api-demos.githubapp.com/updates/'
+  const releaseServer = 'https://example.githubapp.com/updates/'
   let assetName
 
-  if (platform === 'darwin') assetName = 'valex-api-demos-mac.zip'
-  if (platform === 'win32') assetName = 'ValexAPIDemosSetup.exe'
-  if (platform === 'linux') assetName = 'valex-api-demos-linux.zip'
+  if (platform === 'darwin') assetName = 'valex-mac.zip'
+  if (platform === 'win32') assetName = 'valex-win.exe'
+  if (platform === 'linux') assetName = 'valex-linux.zip'
 
   document.querySelector('#download-latest-release')
     .setAttribute('href', releaseServer + assetName)
