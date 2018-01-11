273d40614f3c2c0da281ac8d8a5f23be5ceb82e8
khelmy <khelmy@andrew.cmu.edu>
2018-01-11 18:11:02 -0500

    Rinkeby integration on truffle

--- truffle.js
+++ truffle.js
@@ -1,9 +1,20 @@
+// from: https://blog.abuiles.com/blog/2017/07/09/deploying-truffle-contracts-to-rinkeby/
+// use: geth --rinkeby --rpc --rpcapi db,eth,net,web3,personal --unlock="0x82A88F3edc91324e9Ba52d5231873361d2aE6bb9"
+// change the address to whatever your testnet address is.
+
 module.exports = {
-   networks: {
-   development: {
-   host: "localhost",
-   port: 8545,
-   network_id: "*" // Match any network id
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x82A88F3edc91324e9Ba52d5231873361d2aE6bb9", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
   }
- }
 };
